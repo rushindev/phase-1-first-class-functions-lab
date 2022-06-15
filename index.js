db5b1e67f7156dfc7e9cd7c4d03737ab43eae71c
York Rushin <rushinyork@gmail.com>
2022-06-14 21:57:33 -0700

    commit tree adf0237e950e6b6275ecc0dffe17c070386ef0d6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    const newDrivers = drivers.slice(0, 2)
+    return newDrivers;
+}
+
+function returnLastTwoDrivers(drivers) {
+    const newDrivers = drivers.slice(2, 4)
+    return newDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function () {
+        const newAmount = multiplier * multiplier;
+        return newAmount
+    }
+}
+
+function fareDoubler(fare) {
+    const newFare = fare * 2;
+    return newFare;
+}
+
+function fareTripler(fares) {
+    const newFares = fares * 3;
+    return newFares;
+}
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+}
